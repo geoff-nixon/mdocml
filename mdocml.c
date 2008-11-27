19a5a6e605264fdbc1939c51bab46ac109a9a2fb
Kristaps Dzonsons <kristaps@bsd.lv>
2008-11-27 18:27:50 +0100

    *** empty log message ***

--- mdocml.c
+++ mdocml.c
@@ -1,4 +1,4 @@
-/* $Id: mdocml.c,v 1.11 2008/11/25 12:14:02 kristaps Exp $ */
+/* $Id: mdocml.c,v 1.12 2008/11/26 22:27:07 kristaps Exp $ */
 /*
  * Copyright (c) 2008 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -58,10 +58,16 @@ main(int argc, char *argv[])
 
 	(void)memset(&args, 0, sizeof(struct md_args));
 	
-	while (-1 != (c = getopt(argc, argv, "o:")))
+	while (-1 != (c = getopt(argc, argv, "o:vW")))
 		switch (c) {
 		case ('o'):
 			out = optarg;
+			break;
+		case ('v'):
+			args.verbosity++;
+			break;
+		case ('W'):
+			args.warnings |= MD_WARN_ALL;
 			break;
 		default:
 			usage();
@@ -213,5 +219,5 @@ usage(void)
 {
 	extern char	*__progname;
 
-	(void)printf("usage: %s [-o outfile] [infile]\n", __progname);
+	(void)printf("usage: %s [-vW] [-o outfile] [infile]\n", __progname);
 }

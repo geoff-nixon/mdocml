38da437397b153d8221167b13a400bacdf583033
Kristaps Dzonsons <kristaps@bsd.lv>
2008-11-30 23:41:35 +0200

    Preliminary xml output filter (validate-renamed) done.

--- libmdocml.h
+++ libmdocml.h
@@ -1,4 +1,4 @@
-/* $Id: libmdocml.h,v 1.8 2008/11/27 17:27:50 kristaps Exp $ */
+/* $Id: libmdocml.h,v 1.9 2008/11/28 11:21:12 kristaps Exp $ */
 /*
  * Copyright (c) 2008 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -21,7 +21,7 @@
 
 #include <sys/types.h>
 
-struct 	md_params_valid {
+struct 	md_params_xml {
 	int		 dummy;
 };
 
@@ -30,12 +30,12 @@ struct 	md_params_html4_strict {
 };
 
 union	md_params {
-	struct md_params_valid valid;
+	struct md_params_xml xml;
 	struct md_params_html4_strict html4_strict;
 };
 
 enum	md_type {
-	MD_VALID,		/* Validate input only. */
+	MD_XML,			/* XML. */
 	MD_HTML4_STRICT		/* HTML4.01-strict. */
 };
 

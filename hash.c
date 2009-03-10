b10ecfe32e7e5a455aadc9fded07ececdc99da1e
Kristaps Dzonsons <kristaps@bsd.lv>
2009-03-10 11:20:03 +0100

    Fixed hash lookup regression.

--- hash.c
+++ hash.c
@@ -1,4 +1,4 @@
-/* $Id: hash.c,v 1.7 2009/03/08 11:41:22 kristaps Exp $ */
+/* $Id: hash.c,v 1.8 2009/03/08 20:29:45 kristaps Exp $ */
 /*
  * Copyright (c) 2008 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -130,10 +130,13 @@ mdoc_tokhash_find(const void *arg, const char *tmp)
 	assert(0 == (size_t)slot % sizeof(struct mdoc_macro));
 	slot /= sizeof(struct mdoc_macro);
 
-	if (0 == tmp[2])
-		return(slot);
+	if (mdoc_macronames[slot][0] != tmp[0])
+		return(MDOC_MAX);
+	if (mdoc_macronames[slot][1] != tmp[1])
+		return(MDOC_MAX);
+	if (tmp[2] && mdoc_macronames[slot][2] != tmp[2])
+		return(MDOC_MAX);
 
-	assert(0 == tmp[3]);
-	return(tmp[2] == mdoc_macronames[slot][2] ? slot : MDOC_MAX);
+	return(slot);
 }
 

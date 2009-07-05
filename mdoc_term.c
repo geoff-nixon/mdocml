4a38026745988fc6c61c801fb6eea9174a9eb343
Kristaps Dzonsons <kristaps@bsd.lv>
2009-07-05 22:25:10 +0300

    Backed out "-width indent" and "-width indent-two", which aren't supported by any groff mdoc tmac (and erroneously used in many manuals, hence the confusion).

--- mdoc_term.c
+++ mdoc_term.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_term.c,v 1.19 2009/07/04 11:04:46 kristaps Exp $ */
+/*	$Id: mdoc_term.c,v 1.20 2009/07/04 11:07:34 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -532,10 +532,6 @@ arg_width(const struct mdoc_argv *arg, int pos)
 
 	assert(pos < (int)arg->sz && pos >= 0);
 	assert(arg->value[pos]);
-	if (0 == strcmp(arg->value[pos], "indent"))
-		return(INDENT + 3);
-	if (0 == strcmp(arg->value[pos], "indent-two"))
-		return(INDENT * 2 + 2);
 
 	if (0 == (len = (int)strlen(arg->value[pos])))
 		return(0);

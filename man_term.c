c1f57599e97da664ae71aad894b2412cc8a1a59f
Kristaps Dzonsons <kristaps@bsd.lv>
2009-08-21 11:41:05 +0300

    FreeBSD fix (uqs@spoerlein.net).

--- man_term.c
+++ man_term.c
@@ -1,4 +1,4 @@
-/*	$Id: man_term.c,v 1.26 2009/08/19 11:30:40 kristaps Exp $ */
+/*	$Id: man_term.c,v 1.27 2009/08/20 11:51:07 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -14,6 +14,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <sys/types.h>
+
 #include <assert.h>
 #include <ctype.h>
 #include <err.h>

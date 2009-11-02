87a3997fa78ec0bbb7f1e4e7fb96925d4d0e8eab
Kristaps Dzonsons <kristaps@bsd.lv>
2009-11-02 07:22:46 +0100

    Added mandoc_a2time() for proper date conversion. Fitted TH and Dd handlers to use mandoc_a2time(). Documented date syntax for -man, fixed documentation for -mdoc.

--- libmandoc.h
+++ libmandoc.h
@@ -1,4 +1,4 @@
-/*	$Id: libmandoc.h,v 1.2 2009/10/28 19:21:59 kristaps Exp $ */
+/*	$Id: libmandoc.h,v 1.3 2009/10/30 05:58:37 kristaps Exp $ */
 /*
  * Copyright (c) 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -24,6 +24,11 @@ void		*mandoc_calloc(size_t, size_t);
 char		*mandoc_strdup(const char *);
 void		*mandoc_malloc(size_t);
 void		*mandoc_realloc(void *, size_t);
+time_t		 mandoc_a2time(int, const char *);
+#define		 MTIME_CANONICAL	(1 << 0)
+#define		 MTIME_REDUCED		(1 << 1)
+#define		 MTIME_MDOCDATE		(1 << 2)
+#define		 MTIME_ISO_8601		(1 << 3)
 
 __END_DECLS
 
--- man_validate.c
+++ man_validate.c
@@ -1,4 +1,4 @@
-/*	$Id: man_validate.c,v 1.25 2009/10/18 11:36:37 kristaps Exp $ */
+/*	$Id: man_validate.c,v 1.26 2009/10/24 05:45:05 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -56,7 +56,7 @@ static	v_check	  pres_bline[] = { check_bline, NULL };
 
 static	const struct man_valid man_valids[MAN_MAX] = {
 	{ pres_bline, posts_eq0 }, /* br */
-	{ pres_bline, posts_ge2_le5 }, /* TH */
+	{ pres_bline, posts_ge2_le5 }, /* TH */ /* FIXME: make sure capitalised. */
 	{ pres_bline, posts_sec }, /* SH */
 	{ pres_bline, posts_sec }, /* SS */
 	{ pres_bline, posts_par }, /* TP */

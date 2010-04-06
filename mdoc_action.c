e4d90f6e5a0d7ce8b68a624a02b719b0cb0bddc9
Kristaps Dzonsons <kristaps@bsd.lv>
2010-04-06 13:33:00 +0200

    Migrating mdoc_node_free() and mdoc_node_freelist() to use mdoc_node_delete(), which has a more intuitive interface and mirrors libman.

--- mdoc_action.c
+++ mdoc_action.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_action.c,v 1.51 2010/03/31 07:13:53 kristaps Exp $ */
+/*	$Id: mdoc_action.c,v 1.52 2010/04/05 09:03:12 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -729,7 +729,7 @@ post_bl_head(POST_ARGS)
 		nn->string = NULL;
 		nnp = nn;
 		nn = nn->next;
-		mdoc_node_free(nnp);
+		mdoc_node_delete(NULL, nnp);
 	}
 
 	n->nchild = 0;
@@ -854,29 +854,10 @@ post_dd(POST_ARGS)
 static int
 post_prol(POST_ARGS)
 {
-	struct mdoc_node *np;
 
-	if (n->parent->child == n)
-		n->parent->child = n->prev;
-	if (n->prev)
-		n->prev->next = NULL;
-
-	np = n;
-	assert(NULL == n->next);
-
-	if (n->prev) {
-		m->last = n->prev;
-		m->next = MDOC_NEXT_SIBLING;
-	} else {
-		m->last = n->parent;
-		m->next = MDOC_NEXT_CHILD;
-	}
-
-	mdoc_node_freelist(np);
-
+	mdoc_node_delete(m, n);
 	if (m->meta.title && m->meta.date && m->meta.os)
 		m->flags |= MDOC_PBODY;
-
 	return(1);
 }
 

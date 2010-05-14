6008d590260dc0db3cca76a8af23232050c9ddb8
Kristaps Dzonsons <kristaps@bsd.lv>
2010-05-14 15:54:15 +0200

    Fix for Joerg's patch stripping of pre-comment whitespace (by Joerg).

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.61 2010/04/12 19:27:22 kristaps Exp $ */
+/*	$Id: main.c,v 1.62 2010/05/09 21:19:42 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -375,9 +375,9 @@ fdesc(struct buf *blk, struct buf *ln, struct curparse *curp)
 				comment = 1;
 				pos -= 2;
 				for (; pos > 0; --pos) {
-					if (ln->buf[pos] != ' ')
+					if (ln->buf[pos - 1] != ' ')
 						break;
-					if (ln->buf[pos - 1] == '\\')
+					if (pos > 2 && ln->buf[pos - 2] == '\\')
 						break;
 				}
 				continue;

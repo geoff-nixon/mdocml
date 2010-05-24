2161df6feb122b530d5bf6d0e8a45566df4674b6
joerg <joerg>
2010-05-24 11:59:37 +0000

    If a .Bl -tag lacks a -width, skip all non-It macros for the implicit calculation. E.g. .Sm can occur as direct child of .Bl.
    OK schwarze@

--- mdoc_action.c
+++ mdoc_action.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_action.c,v 1.59 2010/05/17 22:11:42 kristaps Exp $ */
+/*	$Id: mdoc_action.c,v 1.60 2010/05/17 23:57:06 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -637,11 +637,14 @@ post_bl_tagwidth(POST_ARGS)
 	/* Defaults to ten ens. */
 
 	sz = 10; /* XXX: make this a macro value. */
-	nn = n->body->child;
+
+	for (nn = n->body->child; nn; nn = nn->next) {
+		if (MDOC_It == nn->tok)
+			break;
+	}
 
 	if (nn) {
 		assert(MDOC_BLOCK == nn->type);
-		assert(MDOC_It == nn->tok);
 		nn = nn->head->child;
 		if (MDOC_TEXT != nn->type) {
 			sz = mdoc_macro2len(nn->tok);

0267b52d42c3a7f80440be16d38e976cc382f7b9
Kristaps Dzonsons <kristaps@bsd.lv>
2010-06-11 01:56:33 +0200

    Allow open word contexts in -Tps to preserve whitespace, as whitespace apparently doesn't collapse in PostScript (surprise!).  Makes output files much more compact.

--- term_ps.c
+++ term_ps.c
@@ -1,4 +1,4 @@
-/*	$Id: term_ps.c,v 1.4 2010/06/09 08:07:13 kristaps Exp $ */
+/*	$Id: term_ps.c,v 1.5 2010/06/09 08:31:18 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -268,11 +268,12 @@ ps_letter(struct termp *p, char c)
 static void
 ps_advance(struct termp *p, size_t len)
 {
+	size_t		 i;
 
 	if (PS_INLINE & p->engine.ps.psstate) {
-		/* Dump out any existing line scope. */
-		ps_printf(p, ") show\n");
-		p->engine.ps.psstate &= ~PS_INLINE;
+		for (i = 0; i < len; i++) 
+			ps_letter(p, ' ');
+		return;
 	}
 
 	p->engine.ps.pscol += len ? len * PS_CHAR_WIDTH : 0;

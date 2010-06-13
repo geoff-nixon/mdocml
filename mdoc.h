e476a3fa1850ff2722f2a5c36fe28daf09d9b912
Kristaps Dzonsons <kristaps@bsd.lv>
2010-06-13 23:02:49 +0200

    Allow -width for lists to be cached in mdoc_bl.  This requires some trickery because widths may be on-the-fly recalculated.  I don't like how these are split between mdoc_action.c and mdoc_validate.c, but for the time being, it'll do.

--- mdoc.h
+++ mdoc.h
@@ -1,4 +1,4 @@
-/*	$Id: mdoc.h,v 1.87 2010/06/12 12:10:55 kristaps Exp $ */
+/*	$Id: mdoc.h,v 1.88 2010/06/12 12:38:01 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -279,6 +279,7 @@ struct	mdoc_bd {
 };
 
 struct	mdoc_bl {
+	const char	 *width; /* -width */
 	const char	 *offs; /* -offset */
 	enum mdoc_list	  type; /* -tag, -enum, etc. */
 	int		  comp; /* -compact */

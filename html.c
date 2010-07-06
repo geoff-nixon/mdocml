c1434a2ce2fc8df44e223a8f013d0810a568dd89
Kristaps Dzonsons <kristaps@bsd.lv>
2010-07-06 13:10:53 +0200

    Brought function arguments and style in ine with term_word() in term.c to make sharing of TERMP_KEEP easier.

--- html.c
+++ html.c
@@ -1,4 +1,4 @@
-/*	$Id: html.c,v 1.102 2010/06/19 20:46:27 kristaps Exp $ */
+/*	$Id: html.c,v 1.103 2010/06/25 19:50:23 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -484,11 +484,11 @@ print_doctype(struct html *h)
 
 
 void
-print_text(struct html *h, const char *p)
+print_text(struct html *h, const char *word)
 {
 
-	if (*p && 0 == *(p + 1))
-		switch (*p) {
+	if (word[0] && '\0' == word[1])
+		switch (word[0]) {
 		case('.'):
 			/* FALLTHROUGH */
 		case(','):
@@ -514,16 +514,16 @@ print_text(struct html *h, const char *p)
 	if ( ! (h->flags & HTML_NOSPACE))
 		putchar(' ');
 
-	assert(p);
-	if ( ! print_encode(h, p, 0))
+	assert(word);
+	if ( ! print_encode(h, word, 0))
 		h->flags &= ~HTML_NOSPACE;
 
 	/* 
 	 * Note that we don't process the pipe: the parser sees it as
 	 * punctuation, but we don't in terms of typography.
 	 */
-	if (*p && 0 == *(p + 1))
-		switch (*p) {
+	if (word[0] && '\0' == word[1])
+		switch (word[0]) {
 		case('('):
 			/* FALLTHROUGH */
 		case('['):

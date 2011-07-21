cc777c60013a3b9979db69ce5c1d303e29d4c8d4
Kristaps Dzonsons <kristaps@bsd.lv>
2011-07-21 15:37:04 +0200

    Add eqn subexpression fonts.

--- tree.c
+++ tree.c
@@ -1,4 +1,4 @@
-/*	$Id: tree.c,v 1.39 2011/07/21 12:30:44 kristaps Exp $ */
+/*	$Id: tree.c,v 1.40 2011/07/21 13:18:24 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -270,15 +270,18 @@ print_box(const struct eqn_box *ep, int indent)
 
 	switch (ep->type) {
 	case (EQN_ROOT):
-		printf("eqn-root(%d)\n", ep->mark);
+		printf("eqn-root(%d, %d)\n", 
+				ep->font, ep->mark);
 		print_box(ep->child, indent + 1);
 		break;
 	case (EQN_SUBEXPR):
-		printf("eqn-subxpr(%d)\n", ep->mark);
+		printf("eqn-subxpr(%d, %d)\n", 
+				ep->font, ep->mark);
 		print_box(ep->child, indent + 1);
 		break;
 	case (EQN_TEXT):
-		printf("eqn-text(%d): [%s]\n", ep->mark, ep->text);
+		printf("eqn-text(%d, %d): [%s]\n", 
+				ep->font, ep->mark, ep->text);
 		break;
 	default:
 		break;

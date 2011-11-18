6a8e56d8f0f7d9918f693f2c5fc484685fd8cbf4
joerg <joerg>
2011-11-18 17:06:19 +0000

    Fix condition. Not enough tea.

--- man_macro.c
+++ man_macro.c
@@ -1,4 +1,4 @@
-/*	$Id: man_macro.c,v 1.67 2011/11/18 16:39:08 joerg Exp $ */
+/*	$Id: man_macro.c,v 1.68 2011/11/18 16:43:29 joerg Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -120,7 +120,7 @@ man_unscope(struct man *m, const struct man_node *to,
 
 	assert(to);
 
-	if (MAN_ROOT != m->last->type)
+	if (MAN_ROOT == m->last->type)
 		return(1);
 	m->next = MAN_NEXT_SIBLING;
 

ff277b51e914ecf9fc0614fa66d96fe848ea4309
Ingo Schwarze <schwarze@openbsd.org>
2011-12-03 17:08:51 +0100

    ISO style "%Y-%m-%d" dates are common in man(7) .TH. They have been considered valid in the past, but were reformatted to the mdoc(7) "Month day, year" style. To make page footers more similar to groff, no longer reformat them, just print them as they are. This doesn't change anything with respect to what's considered valid or what is warned about.
    ok kristaps@

--- mandoc.c
+++ mandoc.c
@@ -1,4 +1,4 @@
-/*	$Id: mandoc.c,v 1.60 2011/10/24 20:30:57 schwarze Exp $ */
+/*	$Id: mandoc.c,v 1.61 2011/11/06 14:43:14 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011 Ingo Schwarze <schwarze@openbsd.org>
@@ -623,9 +623,10 @@ mandoc_normdate(struct mparse *parse, char *in, int ln, int pos)
 		mandoc_msg(MANDOCERR_NODATE, parse, ln, pos, NULL);
 		time(&t);
 	}
+	else if (a2time(&t, "%Y-%m-%d", in))
+		t = 0;
 	else if (!a2time(&t, "$" "Mdocdate: %b %d %Y $", in) &&
-	    !a2time(&t, "%b %d, %Y", in) &&
-	    !a2time(&t, "%Y-%m-%d", in)) {
+	    !a2time(&t, "%b %d, %Y", in)) {
 		mandoc_msg(MANDOCERR_BADDATE, parse, ln, pos, NULL);
 		t = 0;
 	}

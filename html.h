89a4fda71c575a17bf3993974de88be4edaab7c2
Ingo Schwarze <schwarze@openbsd.org>
2012-06-01 00:29:13 +0200

    Implement the roff \z escape sequence, intended to output the next character without advancing the cursor position; implement it to simply skip the next character, as it will usually be overwritten.
    With this change, the pod2man(1) preamble user-defined string \*:,
    intended to render as a diaeresis or umlaut diacritic above the
    preceding character, is rendered in a slightly less ugly way,
    though still not correctly.  It was rendered as "z.." and is now
    rendered as ".".
    
    Given that the definition of \*: uses elaborate manual \h positioning,
    there is little chance for mandoc(1) to ever render it correctly,
    but at least we can refrain from printing out a spurious "z", and
    we can make the \z do something semi-reasonable for easier cases.
    
    "just commit" kristaps@

--- html.h
+++ html.h
@@ -1,4 +1,4 @@
-/*	$Id: html.h,v 1.46 2011/09/18 14:14:15 schwarze Exp $ */
+/*	$Id: html.h,v 1.47 2011/10/05 21:35:17 kristaps Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -117,6 +117,7 @@ struct	html {
 #define	HTML_PREKEEP	 (1 << 3)
 #define	HTML_NONOSPACE	 (1 << 4) /* never add spaces */
 #define	HTML_LITERAL	 (1 << 5) /* literal (e.g., <PRE>) context */
+#define	HTML_SKIPCHAR	 (1 << 6) /* skip the next character */
 	struct tagq	  tags; /* stack of open tags */
 	struct rofftbl	  tbl; /* current table */
 	struct tag	 *tblt; /* current open table scope */

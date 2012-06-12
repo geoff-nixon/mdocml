ddcd02057767915856421e79c16b8fb4378a5cb4
Kristaps Dzonsons <kristaps@bsd.lv>
2012-06-12 15:47:14 +0300

    Fix an assert() raised by `RS' when following `TP'. The reason was that `RS' wasn't BSCOPE'd, so the next-line (BLINE) scope opened by `TP' would still be in the HEAD macro. This was from joerg@'s archive of failures.

--- man_macro.c
+++ man_macro.c
@@ -1,4 +1,4 @@
-/*	$Id: man_macro.c,v 1.72 2012/06/02 20:16:23 schwarze Exp $ */
+/*	$Id: man_macro.c,v 1.73 2012/06/03 09:52:07 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2012 Ingo Schwarze <schwarze@openbsd.org>
@@ -78,7 +78,7 @@ const	struct man_macro __man_macros[MAN_MAX] = {
 	{ in_line_eoln, MAN_BSCOPE }, /* nf */
 	{ in_line_eoln, MAN_BSCOPE }, /* fi */
 	{ blk_close, 0 }, /* RE */
-	{ blk_exp, MAN_EXPLICIT }, /* RS */
+	{ blk_exp, MAN_BSCOPE | MAN_EXPLICIT }, /* RS */
 	{ in_line_eoln, 0 }, /* DT */
 	{ in_line_eoln, 0 }, /* UC */
 	{ in_line_eoln, 0 }, /* PD */

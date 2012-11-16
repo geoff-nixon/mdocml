7a2952d0321273985e8529e50df6043a967ccf01
Ingo Schwarze <schwarze@openbsd.org>
2012-11-16 14:40:36 +0100

    Fix a crash triggered by .Bl -tag .It Xo .El .Sh found by florian@.
    * When allocating a body end marker, copy the pointer to the normalized
    block information from the body block, avoiding the risk of subsequent
    null pointer derefence.
    * When inserting the body end marker into the syntax tree, do not try to
    copy that pointer from the parent block, because not being a direkt child
    of the block it belongs to is the whole point of a body end marker.
    * Even non-callable blocks (like Bd and Bl) can break other blocks;
    when this happens, postpone closing them out in the usual way.
    
    Completed and tested at the OpenBSD impromptu Coimbra hackathon (c2k12).
    Thanks to Pedro Almeida and the Laborat'orio de Computa,c~ao Avan,cada
    da Universidade de Coimbra (http://www.uc.pt/lca) for their hospitality!

--- mdoc_macro.c
+++ mdoc_macro.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_macro.c,v 1.116 2012/05/27 17:39:28 schwarze Exp $ */
+/*	$Id: mdoc_macro.c,v 1.117 2012/07/18 16:20:43 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010, 2012 Ingo Schwarze <schwarze@openbsd.org>
@@ -738,7 +738,7 @@ blk_exp_close(MACRO_PROT_ARGS)
 		if (later &&
 		    MDOC_EXPLICIT & mdoc_macros[later->tok].flags)
 			continue;
-		if (MDOC_CALLABLE & mdoc_macros[n->tok].flags)
+		if (MDOC_It != n->tok)
 			later = n;
 	}
 

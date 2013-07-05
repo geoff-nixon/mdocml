ac2164af27a34d40a3062debd01da7e2ea414e0a
Ingo Schwarze <schwarze@openbsd.org>
2013-07-05 11:33:02 +0200

    Simple partial sync with OpenBSD: * Again accept suffixes on the name of the whatis utility. * The usage line for whatis must not invite expressions. * Revert the argument names in the SYNOPSIS back to the usual ones. * Revert a few gratuitious changes regarding line breaks etc.

--- apropos.c
+++ apropos.c
@@ -1,4 +1,4 @@
-/*	$Id: apropos.c,v 1.32 2012/06/09 14:11:15 kristaps Exp $ */
+/*	$Id: apropos.c,v 1.33 2012/06/09 17:49:13 kristaps Exp $ */
 /*
  * Copyright (c) 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -37,8 +37,9 @@ main(int argc, char *argv[])
 	struct mansearch search;
 	size_t		 i, sz;
 	struct manpage	*res;
-	char		*conf_file, *defpaths, *auxpaths;
 	struct manpaths	 paths;
+	char		*defpaths, *auxpaths;
+	char		*conf_file;
 	char		*progname;
 	extern char	*optarg;
 	extern int	 optind;
@@ -49,10 +50,13 @@ main(int argc, char *argv[])
 	else
 		++progname;
 
-	auxpaths = defpaths = conf_file = NULL;
+	whatis = (0 == strncmp(progname, "whatis", 6));
+
 	memset(&paths, 0, sizeof(struct manpaths));
 	memset(&search, 0, sizeof(struct mansearch));
-	whatis = (0 == strcmp(progname, "whatis"));
+
+	auxpaths = defpaths = NULL;
+	conf_file = NULL;
 
 	while (-1 != (ch = getopt(argc, argv, "C:M:m:S:s:")))
 		switch (ch) {
@@ -99,12 +103,8 @@ main(int argc, char *argv[])
 	free(res);
 	return(sz ? EXIT_SUCCESS : EXIT_FAILURE);
 usage:
-	fprintf(stderr, "usage: %s [-C conf] "
-			 	  "[-M paths] "
-				  "[-m paths] "
-				  "[-S arch] "
-				  "[-s section] "
-			          "expr ...\n", 
-				  progname);
+	fprintf(stderr, "usage: %s [-C file] [-M path] [-m path] "
+			"[-S arch] [-s section]%s ...\n", progname,
+			whatis ? " name" : "\n               expression");
 	return(EXIT_FAILURE);
 }

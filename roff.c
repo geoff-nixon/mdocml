11455e1ae72d50d9ac6ab090cdc1d992fed079e0
Ingo Schwarze <schwarze@openbsd.org>
2013-10-05 22:30:05 +0200

    Cleanup suggested by gcc-4.8.1, following hints by Christos Zoulas: - avoid bad qualifier casting in roff.c, roff_parsetext()   by changing the mandoc_escape arguments to "const char const **" - avoid bad qualifier casting in mandocdb.c, index_merge() - do not complain about unused variables in test-*.c - garbage collect a few unused variables elsewhere

--- roff.c
+++ roff.c
@@ -1,4 +1,4 @@
-/*	$Id: roff.c,v 1.177 2013/06/27 09:49:47 schwarze Exp $ */
+/*	$Id: roff.c,v 1.178 2013/07/13 12:52:07 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010, 2011, 2012, 2013 Ingo Schwarze <schwarze@openbsd.org>
@@ -627,7 +627,7 @@ roff_parsetext(char **bufp, size_t *szp, int pos, int *offs)
 			/* Skip over escapes. */
 			p++;
 			esc = mandoc_escape
-				((const char **)&p, NULL, NULL);
+				((const char const **)&p, NULL, NULL);
 			if (ESCAPE_ERROR == esc)
 				break;
 			continue;

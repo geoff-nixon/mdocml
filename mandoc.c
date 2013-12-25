81e069b718d799505a68d779d0031d1594dee79a
Ingo Schwarze <schwarze@openbsd.org>
2013-12-25 23:45:33 +0100

    Parse and ignore the roff(7) escape sequences \d (move half line down) und \u (move half line up).  Found by bentley@ in some DocBook crap.

--- mandoc.c
+++ mandoc.c
@@ -1,4 +1,4 @@
-/*	$Id: mandoc.c,v 1.70 2013/11/10 21:34:04 schwarze Exp $ */
+/*	$Id: mandoc.c,v 1.71 2013/12/25 00:50:05 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2013 Ingo Schwarze <schwarze@openbsd.org>
@@ -100,6 +100,14 @@ mandoc_escape(const char const **end, const char const **start, int *sz)
 			gly = ESCAPE_SPECIAL;
 		term = '\'';
 		break;
+
+	/*
+	 * Escapes taking no arguments at all.
+	 */
+	case ('d'):
+		/* FALLTHROUGH */
+	case ('u'):
+		return(ESCAPE_IGNORE);
 
 	/*
 	 * The \z escape is supposed to output the following

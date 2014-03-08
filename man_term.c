33179e6ace32e1ac8ba5a847577fd99ba5da04c8
Ingo Schwarze <schwarze@openbsd.org>
2014-03-08 17:22:04 +0100

    In .nf mode, use the MAN_LINE flag to detect input line breaks instead of the man_node line member.  This is required to preserve line breaks contained in user-defined macros called in .nf mode. Found in a code audit triggered by fixing a similar issue in .TP.

--- man_term.c
+++ man_term.c
@@ -1,4 +1,4 @@
-/*	$Id: man_term.c,v 1.140 2014/02/16 12:33:39 schwarze Exp $ */
+/*	$Id: man_term.c,v 1.141 2014/03/08 15:50:41 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -1045,7 +1045,7 @@ out:
 	 * more specific than this.
 	 */
 	if (MANT_LITERAL & mt->fl && ! (TERMP_NOBREAK & p->flags) &&
-	    (NULL == n->next || n->next->line > n->line)) {
+	    (NULL == n->next || MAN_LINE & n->next->flags)) {
 		rm = p->rmargin;
 		rmax = p->maxrmargin;
 		p->rmargin = p->maxrmargin = TERM_MAXMARGIN;

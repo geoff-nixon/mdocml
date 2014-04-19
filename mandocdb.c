0d8d4855f3ae313c6877a8ca1c3930880749ffc4
Ingo Schwarze <schwarze@openbsd.org>
2014-04-19 04:56:54 +0200

    Two minor tweaks regarding the fallback from -u/-d to default mode: (1) Use all files found on the command line, but do *not* use all stray files found during fallback tree recursion. (2) If the fallback works, call that success, i.e. exit(0). As pointed out by naddy@, the latter is required for ports' happiness.

--- mandocdb.c
+++ mandocdb.c
@@ -1,4 +1,4 @@
-/*	$Id: mandocdb.c,v 1.138 2014/04/18 21:55:38 schwarze Exp $ */
+/*	$Id: mandocdb.c,v 1.139 2014/04/19 02:30:19 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2013, 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -430,19 +430,20 @@ main(int argc, char *argv[])
 	ohash_init(&mlinks, 6, &mlinks_info);
 
 	if (OP_UPDATE == op || OP_DELETE == op || OP_TEST == op) {
-		/* 
-		 * Force processing all files.
-		 */
-		use_all = 1;
 
 		/*
 		 * All of these deal with a specific directory.
-		 * Jump into that directory then collect files specified
-		 * on the command-line.
+		 * Jump into that directory first.
 		 */
 		if (0 == set_basedir(path_arg))
 			goto out;
+
 		if (dbopen(1)) {
+			/*
+			 * The existing database is usable.  Process
+			 * all files specified on the command-line.
+			 */
+			use_all = 1;
 			for (i = 0; i < argc; i++)
 				filescan(argv[i]);
 			if (OP_TEST != op)
@@ -452,6 +453,7 @@ main(int argc, char *argv[])
 			 * Database missing or corrupt.
 			 * Recreate from scratch.
 			 */
+			exitcode = (int)MANDOCLEVEL_OK;
 			op = OP_DEFAULT;
 			if (0 == treescan())
 				goto out;

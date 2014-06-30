74c3153a2aabf4ff103677b108ca1e340dc3d410
Ingo Schwarze <schwarze@openbsd.org>
2014-07-01 01:45:08 +0200

    garbage collect two unused enum mandocerr items and fix a couple of comments while here

--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.51 2014/06/21 22:24:01 schwarze Exp $ */
+/*	$Id: read.c,v 1.52 2014/06/25 00:20:19 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -153,9 +153,6 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"bad escape sequence",
 	"unterminated quoted string",
 
-	/* related to equations */
-	"unexpected literal in equation",
-
 	"generic error",
 
 	/* related to equations */
@@ -194,7 +191,6 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"request requires a numeric argument",
 	"missing list type",
 	"line argument(s) will be lost",
-	"body argument(s) will be lost",
 
 	"generic fatal error",
 

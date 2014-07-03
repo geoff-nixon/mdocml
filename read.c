fc863760c11879d5088f8e796b14f2607925bb60
Ingo Schwarze <schwarze@openbsd.org>
2014-07-04 01:24:56 +0200

    Fix formatting of empty .Bl -inset item heads. Downgrade empty item heads from ERROR to WARNING. Show the list type in the error message. Choose better variable names for nodes in post_it().

--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.57 2014/07/02 13:10:45 schwarze Exp $ */
+/*	$Id: read.c,v 1.58 2014/07/03 21:23:54 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -135,6 +135,7 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"missing display type",
 	"list type must come first",
 	"tag lists require a width argument",
+	"missing head in list item",
 	"missing font type",
 
 	/* related to bad macro arguments */
@@ -189,7 +190,6 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"missing end of block",
 	"scope open on exit",
 	"uname(3) system call failed",
-	"macro requires line argument(s)",
 	"macro requires body argument(s)",
 	"macro requires argument(s)",
 	"request requires a numeric argument",

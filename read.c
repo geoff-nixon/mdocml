b28b2007aa11a2514878c07d71a6b7e822869f20
Ingo Schwarze <schwarze@openbsd.org>
2014-07-05 03:12:20 +0200

    Cleanup regarding -offset and -width: * Bugfix: Last one wins, not first one. * Fix .Bl -width without argument: it means 0n, so do not ignore it. * Report macro names, argument names and fallbacks in related messages. * Simplify: Garbage collect auxiliary variables in pre_bd() and pre_bl().

--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.60 2014/07/04 01:50:07 schwarze Exp $ */
+/*	$Id: read.c,v 1.61 2014/07/04 16:12:08 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -131,6 +131,7 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"skipping empty request",
 	"conditional request controls empty scope",
 	"skipping empty macro",
+	"empty argument, using 0n",
 	"argument count wrong",
 	"missing display type, using -ragged",
 	"list type is not the first argument",

77f14b229319c8a0876ed5ab6920200b5ebbf57a
Ingo Schwarze <schwarze@openbsd.org>
2014-07-07 13:35:06 +0200

    implement .dei and .ami

--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.63 2014/07/05 12:34:17 schwarze Exp $ */
+/*	$Id: read.c,v 1.64 2014/07/06 19:09:00 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -185,7 +185,6 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"manual name not yet set",
 	"skipping text before first section header",
 	"skipping unknown macro",
-	"NOT IMPLEMENTED, please use groff: skipping request",
 	"argument count wrong",
 	"skipping invalid content in .Rs block",
 	"unknown standard specifier",

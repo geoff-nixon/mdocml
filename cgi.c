17282d59023534d7af1c7d72a3da0de5ba41701d
Ingo Schwarze <schwarze@openbsd.org>
2014-07-12 03:55:30 +0200

    fix the two manual links on the index page

--- cgi.c
+++ cgi.c
@@ -1,4 +1,4 @@
-/*	$Id: cgi.c,v 1.62 2014/07/11 22:25:44 schwarze Exp $ */
+/*	$Id: cgi.c,v 1.63 2014/07/11 22:27:35 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014 Ingo Schwarze <schwarze@usta.de>
@@ -415,13 +415,14 @@ resp_index(const struct req *req)
 	     "<A HREF=\"http://mdocml.bsd.lv/\">mandoc</A>\n"
 	     "</H1>");
 	resp_searchform(req);
-	puts("<P>\n"
-	     "This web interface is documented in the "
-	     "<A HREF=\"search?expr=Nm~^man\\.cgi$&amp;sec=8\">"
-	     "man.cgi</A> manual, and the "
-	     "<A HREF=\"search?expr=Nm~^apropos$&amp;sec=1\">"
-	     "apropos</A> manual explains the query syntax.\n"
-	     "</P>");
+	printf("<P>\n"
+	       "This web interface is documented in the "
+	       "<A HREF=\"%s/search?expr=Nm~^man\\.cgi$&amp;sec=8\">"
+	       "man.cgi</A> manual, and the "
+	       "<A HREF=\"%s/search?expr=Nm~^apropos$&amp;sec=1\">"
+	       "apropos</A> manual explains the query syntax.\n"
+	       "</P>\n",
+	       scriptname, scriptname);
 	resp_end_html();
 }
 

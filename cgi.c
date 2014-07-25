4c1a753ea1a4d5d570880b98077f9b89513f556a
Ingo Schwarze <schwarze@openbsd.org>
2014-07-25 20:20:39 +0200

    We cannot easily control the order of the QUERY_STRING keys generated by the search form, it's just the order of the fields in the form. Actually, that's not too bad; the generated URI resembles the generating form.
    To minimize confusion for people looking at URIs, give the keys
    in the same order when generating URIs for search listings and
    search redirections, the latter being used instead of search
    listings that would have only one single entry.  Also, if the
    manpath is the default, remove it form the generated URIs.

--- cgi.c
+++ cgi.c
@@ -1,4 +1,4 @@
-/*	$Id: cgi.c,v 1.86 2014/07/25 17:34:06 schwarze Exp $ */
+/*	$Id: cgi.c,v 1.87 2014/07/25 17:52:04 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014 Ingo Schwarze <schwarze@usta.de>
@@ -148,10 +148,12 @@ static void
 http_printquery(const struct req *req)
 {
 
-	if (NULL != req->q.manpath) {
-		printf("&manpath=");
-		http_print(req->q.manpath);
+	if (NULL != req->q.query) {
+		printf("query=");
+		http_print(req->q.query);
 	}
+	if (0 == req->q.equal)
+		printf("&apropos=1");
 	if (NULL != req->q.sec) {
 		printf("&sec=");
 		http_print(req->q.sec);
@@ -160,22 +162,23 @@ http_printquery(const struct req *req)
 		printf("&arch=");
 		http_print(req->q.arch);
 	}
-	if (NULL != req->q.query) {
-		printf("&query=");
-		http_print(req->q.query);
+	if (NULL != req->q.manpath &&
+	    strcmp(req->q.manpath, req->p[0])) {
+		printf("&manpath=");
+		http_print(req->q.manpath);
 	}
-	if (0 == req->q.equal)
-		printf("&apropos=1");
 }
 
 static void
 html_printquery(const struct req *req)
 {
 
-	if (NULL != req->q.manpath) {
-		printf("&amp;manpath=");
-		html_print(req->q.manpath);
+	if (NULL != req->q.query) {
+		printf("query=");
+		html_print(req->q.query);
 	}
+	if (0 == req->q.equal)
+		printf("&amp;apropos=1");
 	if (NULL != req->q.sec) {
 		printf("&amp;sec=");
 		html_print(req->q.sec);
@@ -184,12 +187,11 @@ html_printquery(const struct req *req)
 		printf("&amp;arch=");
 		html_print(req->q.arch);
 	}
-	if (NULL != req->q.query) {
-		printf("&amp;query=");
-		html_print(req->q.query);
+	if (NULL != req->q.manpath &&
+	    strcmp(req->q.manpath, req->p[0])) {
+		printf("&amp;manpath=");
+		html_print(req->q.manpath);
 	}
-	if (0 == req->q.equal)
-		printf("&amp;apropos=1");
 }
 
 static void

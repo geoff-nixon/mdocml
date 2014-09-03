fbbf545d757aadde63e4b216b08780b49a21f5ad
Ingo Schwarze <schwarze@openbsd.org>
2014-09-03 07:22:45 +0200

    Implement the traditional -h option for man(1): show the SYNOPSIS only. As usual, we get mandoc -h and apropos -h for free. Try stuff like "apropos -h In=dirent" or "apropos -h Fa=timespec".
    Only useful for terminal output, so -Tps, -Tpdf, -Thtml ignore -h for now.

--- term.h
+++ term.h
@@ -1,4 +1,4 @@
-/*	$Id: term.h,v 1.100 2014/04/08 07:13:12 schwarze Exp $ */
+/*	$Id: term.h,v 1.101 2014/04/20 16:46:05 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2013, 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -54,6 +54,7 @@ struct	termp_tbl {
 struct	termp {
 	enum termtype	  type;
 	struct rofftbl	  tbl;		/* table configuration */
+	int		  synopsisonly; /* print the synopsis only */
 	int		  mdocstyle;	/* imitate mdoc(7) output */
 	size_t		  defindent;	/* Default indent for text. */
 	size_t		  defrmargin;	/* Right margin of the device. */

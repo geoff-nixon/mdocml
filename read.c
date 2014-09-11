8ae5cbd328543e19ba1e58a6d929eb3000aafca9
Ingo Schwarze <schwarze@openbsd.org>
2014-09-12 01:53:30 +0200

    warn about botched .Xr ordering and punctuation below SEE ALSO; inspired by mdoclint(1)

--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.85 2014/09/07 02:17:40 schwarze Exp $ */
+/*	$Id: read.c,v 1.86 2014/09/07 23:25:01 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -117,6 +117,8 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"sections out of conventional order",
 	"duplicate section title",
 	"unexpected section",
+	"unusual Xr order",
+	"unusual Xr punctuation",
 	"AUTHORS section without An macro",
 
 	/* related to macros and nesting */

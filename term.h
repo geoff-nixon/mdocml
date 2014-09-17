a1af769974df2c6688da4316a938baf5d54aa68b
Ingo Schwarze <schwarze@openbsd.org>
2014-09-17 22:18:58 +0200

    simplify handling of .An -[no]split for terminal output: delete one static function, one flag #define, and 25 lines of code; no functional change

--- term.h
+++ term.h
@@ -1,4 +1,4 @@
-/*	$Id: term.h,v 1.101 2014/04/20 16:46:05 schwarze Exp $ */
+/*	$Id: term.h,v 1.102 2014/09/03 05:22:45 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2013, 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -81,9 +81,8 @@ struct	termp {
 #define	TERMP_BRIND	 (1 << 9)	/* See term_flushln(). */
 #define	TERMP_DANGLE	 (1 << 10)	/* See term_flushln(). */
 #define	TERMP_HANG	 (1 << 11)	/* See term_flushln(). */
-#define	TERMP_NOSPLIT	 (1 << 12)	/* See termp_an_pre/post(). */
-#define	TERMP_SPLIT	 (1 << 13)	/* See termp_an_pre/post(). */
-#define	TERMP_ANPREC	 (1 << 14)	/* See termp_an_pre(). */
+#define	TERMP_NOSPLIT	 (1 << 12)	/* Do not break line before .An. */
+#define	TERMP_SPLIT	 (1 << 13)	/* Break line before .An. */
 	int		 *buf;		/* Output buffer. */
 	enum termenc	  enc;		/* Type of encoding. */
 	struct mchars	 *symtab;	/* Encoded-symbol table. */

75938472916a4055d67eceb3f70250bdc5615fcf
Ingo Schwarze <schwarze@openbsd.org>
2014-11-27 02:58:21 +0100

    Make makewhatis(8) understand .so links to .gz pages. Drop the FORM_GZ annotation in the mpages table; it is conceptually wrong because it ought to be in the mlinks table: An uncompressed .so link file can point to a compressed manual page file and vice versa. Besides, it is no longer needed because mparse_open() handles it all. Sprinkle some KNF while here.

--- mansearch.h
+++ mansearch.h
@@ -1,4 +1,4 @@
-/*	$Id: mansearch.h,v 1.19 2014/11/11 19:04:55 schwarze Exp $ */
+/*	$Id: mansearch.h,v 1.20 2014/11/18 01:15:21 schwarze Exp $ */
 /*
  * Copyright (c) 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2013, 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -70,7 +70,6 @@
 
 #define	FORM_CAT	 0  /* manual page is preformatted */
 #define	FORM_SRC	 1  /* format is mdoc(7) or man(7) */
-#define	FORM_GZ		 2  /* compressed with gzip(1) */
 #define	FORM_NONE	 4  /* format is unknown */
 
 enum	argmode {

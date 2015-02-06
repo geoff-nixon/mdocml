d57647d70b84815db163de6f99d16c13fb8c33a7
Ingo Schwarze <schwarze@openbsd.org>
2015-02-06 12:54:36 +0100

    better error reporting regarding .OP .RS .UR .TH arguments

--- mandoc.h
+++ mandoc.h
@@ -1,4 +1,4 @@
-/*	$Id: mandoc.h,v 1.197 2015/02/06 03:38:45 schwarze Exp $ */
+/*	$Id: mandoc.h,v 1.198 2015/02/06 07:13:14 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2011, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -106,6 +106,8 @@ enum	mandocerr {
 	MANDOCERR_PF_SKIP, /* nothing follows prefix: Pf arg */
 	MANDOCERR_RS_EMPTY, /* empty reference block: Rs */
 	MANDOCERR_ARG_STD, /* missing -std argument, adding it: macro */
+	MANDOCERR_OP_EMPTY, /* missing option string, using "": OP */
+	MANDOCERR_UR_NOHEAD, /* missing resource identifier, using "": UR */
 	MANDOCERR_EQN_NOBOX, /* missing eqn box, using "": op */
 
 	/* related to bad arguments */
--- read.c
+++ read.c
@@ -1,4 +1,4 @@
-/*	$Id: read.c,v 1.123 2015/02/06 03:38:45 schwarze Exp $ */
+/*	$Id: read.c,v 1.124 2015/02/06 07:13:14 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -150,6 +150,8 @@ static	const char * const	mandocerrs[MANDOCERR_MAX] = {
 	"nothing follows prefix",
 	"empty reference block",
 	"missing -std argument, adding it",
+	"missing option string, using \"\"",
+	"missing resource identifier, using \"\"",
 	"missing eqn box, using \"\"",
 
 	/* related to bad macro arguments */

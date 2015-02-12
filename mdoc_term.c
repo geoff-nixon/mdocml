27312dc0ef38319bcde78928c754f00a72aa9c70
Ingo Schwarze <schwarze@openbsd.org>
2015-02-12 13:24:33 +0100

    Delete the mdoc_node.pending pointer and the function calculating it, make_pending(), which was the most difficult function of the whole mdoc(7) parser.  After almost five years of maintaining this hellhole, i just noticed the pointer isn't needed after all.
    Blocks are always rewound in the reverse order they were opened;
    that even holds for broken blocks.  Consequently, it is sufficient
    to just mark broken blogs with the flag MDOC_BROKEN and breaking
    blocks with the flag MDOC_ENDED.  When rewinding, instead of iterating
    the pending pointers, just iterate from each broken block to its
    parents, rewinding all that are MDOC_ENDED and stopping after
    processing the first ancestor that it not MDOC_BROKEN.  For ENDBODY
    markers, use the mdoc_node.body pointer in place of the former
    mdoc_node.pending.
    
    This also fixes an assertion failure found by jsg@ with afl,
    test case #467 (Bo Bl It Bd Bc It), where (surprise surprise)
    the pending pointer got corrupted.
    
    Improved functionality, minus one function, minus one struct field,
    minus 50 lines of code.

--- mdoc_term.c
+++ mdoc_term.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_term.c,v 1.308 2015/02/05 01:46:56 schwarze Exp $ */
+/*	$Id: mdoc_term.c,v 1.309 2015/02/11 14:15:12 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010, 2012-2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -309,6 +309,7 @@ print_mdoc_node(DECL_ARGS)
 	chld = 1;
 	offset = p->offset;
 	rmargin = p->rmargin;
+	n->flags &= ~MDOC_ENDED;
 	n->prev_font = p->fonti;
 
 	memset(&npair, 0, sizeof(struct termpair));
@@ -361,7 +362,7 @@ print_mdoc_node(DECL_ARGS)
 		print_mdoc_nodelist(p, &npair, meta, n->child);
 
 	term_fontpopq(p,
-	    (ENDBODY_NOT == n->end ? n : n->pending)->prev_font);
+	    (ENDBODY_NOT == n->end ? n : n->body)->prev_font);
 
 	switch (n->type) {
 	case MDOC_TEXT:
@@ -381,7 +382,7 @@ print_mdoc_node(DECL_ARGS)
 		 * that it must not call the post handler again.
 		 */
 		if (ENDBODY_NOT != n->end)
-			n->pending->flags |= MDOC_ENDED;
+			n->body->flags |= MDOC_ENDED;
 
 		/*
 		 * End of line terminating an implicit block

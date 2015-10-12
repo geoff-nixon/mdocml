82f3866d09c96897fab894accf873499f50724e4
Ingo Schwarze <schwarze@openbsd.org>
2015-10-12 23:09:54 +0200

    Check the right pointer against NULL; fixing a pasto introduced in the previous commit; found by Svyatoslav Mishyn <juef at openmailbox dot org> with cppcheck.

--- mandoc_aux.c
+++ mandoc_aux.c
@@ -1,4 +1,4 @@
-/*	$Id: mandoc_aux.c,v 1.5 2015/10/06 18:32:19 schwarze Exp $ */
+/*	$Id: mandoc_aux.c,v 1.6 2015/10/11 21:12:54 schwarze Exp $ */
 /*
  * Copyright (c) 2009, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -95,7 +95,7 @@ mandoc_strdup(const char *ptr)
 	char	*p;
 
 	p = strdup(ptr);
-	if (ptr == NULL)
+	if (p == NULL)
 		err((int)MANDOCLEVEL_SYSERR, NULL);
 	return p;
 }

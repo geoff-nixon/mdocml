bb607ddb09dd18a08ddd05d1ec5e8db538c4eadf
Ingo Schwarze <schwarze@openbsd.org>
2015-10-22 23:03:43 +0200

    If no output device was allocated because no file wanted to produce output, refrain from dereferencing a NULL pointer during final deallocation. Fixing a recent regression reported by czarkoff@

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.250 2015/10/19 19:51:22 schwarze Exp $ */
+/*	$Id: main.c,v 1.251 2015/10/20 02:01:31 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2012, 2014, 2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -466,21 +466,23 @@ main(int argc, char *argv[])
 			mparse_reset(curp.mp);
 	}
 
-	switch (curp.outtype) {
-	case OUTT_HTML:
-		html_free(curp.outdata);
-		break;
-	case OUTT_UTF8:
-	case OUTT_LOCALE:
-	case OUTT_ASCII:
-		ascii_free(curp.outdata);
-		break;
-	case OUTT_PDF:
-	case OUTT_PS:
-		pspdf_free(curp.outdata);
-		break;
-	default:
-		break;
+	if (curp.outdata != NULL) {
+		switch (curp.outtype) {
+		case OUTT_HTML:
+			html_free(curp.outdata);
+			break;
+		case OUTT_UTF8:
+		case OUTT_LOCALE:
+		case OUTT_ASCII:
+			ascii_free(curp.outdata);
+			break;
+		case OUTT_PDF:
+		case OUTT_PS:
+			pspdf_free(curp.outdata);
+			break;
+		default:
+			break;
+		}
 	}
 	mparse_free(curp.mp);
 	mchars_free();
